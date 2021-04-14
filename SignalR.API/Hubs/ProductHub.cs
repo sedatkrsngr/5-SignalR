5a75445c493252a516b52ee8183956e864bdc933
sedat karasungur <limitless_fearless@hotmail.com>
2021-04-14 19:27:38 +0300

    StronglyTypedHubs Bitti

---
+++ SignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product product)
+        {
+            await Clients.All.ProductGonder(product);//İnterface ile yapacağımız metodları belirlersek  //await Clients.All.SendAsync("ProductGonder", product); gibi string yerine metodumuzu direkt yaabiliriz ve daha hatasız bir şekilde ilerleyebiliriz. Tüm Clieent isteklerinde metod olarak belirlenir böylecek. örn. Clilents.Group.ProductGonder veya Caller.ProductGonder ...
+
+
+        }
+    }
+}
